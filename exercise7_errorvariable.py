159b94c8076f0ae2f84d3f4c41713b6d54b6c61d
gauravahluwalia <gauravindrajitahluwalia@gmail.com>
2019-10-12 19:13:04 -0400

    Exercise 7 to induce illegal variable name error

---
+++ exercise7_errorvariable.py
@@ -0,0 +1,13 @@
+def exercise7():
+    3a = 3  # Variable Name Cannot Start with a Number
+    print(3a)
+
+
+exercise7()
+
+def exercise7_1():
+    for = "wrong"  # Variable Name Cannot be Reserved Word
+    print (for)
+
+
+exercise7_1()
