21c09c28fab8b59abee9b16030f516d58e6dc3d3
gauravahluwalia <gauravindrajitahluwalia@gmail.com>
2019-10-12 18:59:03 -0400

    Exercise 6 to print the sentence "I Shall Master the Python Language" as 1 sentence and as a raw text

---
+++ exercise6_master_python.py
@@ -0,0 +1,15 @@
+def exercise6():
+    # Printing as 2 Sentences
+    a = '"I shall master \n the Python Language"'
+    print(a)
+
+
+exercise6()
+
+
+def raw_exercise6(message):  # Raw Text to Print as 1 Sentence
+    print(message)
+
+
+raw_exercise6(r'"I Shall Master \n the Python Language"')
+
